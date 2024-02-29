6b20ce89225a2994b641159b45ff374df8a8d644
klenitskays <klenitskaya0916@gmail.com>
2024-02-29 14:43:16 +0300

    Add passenger-service v.1

--- driver-service/src/main/java/com/example/driver/DriverApplication.java
+++ driver-service/src/main/java/com/example/driver/DriverApplication.java
@@ -1,15 +1,10 @@
 package com.example.driver;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableConfigurationProperties(LiquibaseProperties.class)
@@ -19,15 +14,4 @@ public class DriverApplication {
 		SpringApplication.run(DriverApplication.class, args);
 	}
 
-	@Bean
-	@Primary
-	@Qualifier("driverDataSource")
-	public DataSource driverDataSource() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("org.postgresql.Driver");
-		dataSource.setUrl("jdbc:postgresql://localhost:5442/driverdb");
-		dataSource.setUsername("postgres");
-		dataSource.setPassword("postgre");
-		return dataSource;
-	}
 }
--- passenger-service/src/main/java/com/example/passenger/PassengerApplication.java
+++ passenger-service/src/main/java/com/example/passenger/PassengerApplication.java
@@ -1,28 +1,12 @@
 package com.example.passenger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-import javax.sql.DataSource;
 
 @SpringBootApplication
-@EnableConfigurationProperties(LiquibaseProperties.class)
 public class PassengerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PassengerApplication.class, args);
 	}
 
-	@Bean(name = "dataSource")
-	public DataSource passengerDataSource() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName("org.postgresql.Driver");
-		dataSource.setUrl("jdbc:postgresql://localhost:5452/passengerdb");
-		dataSource.setUsername("postgres");
-		dataSource.setPassword("postgre");
-		return dataSource;
-	}
 }
